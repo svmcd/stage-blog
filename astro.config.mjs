c73aa8e3f68a35504df8849b1a78be7d3230b9a0
svmcd <spolat0750@gmail.com>
2023-04-14 15:49:44 +0200

    installed auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), auth()]
 });
