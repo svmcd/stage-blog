b794a0d460f9318011375a9b5788037db6fb9cf6
svmcd <spolat0750@gmail.com>
2023-04-14 17:16:04 +0200

    installed vercel & configured oa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import auth from "auth-astro";
+import { loadEnv } from 'vite';
+import GitHub from '@auth/core/providers/github';
+import vercel from "@astrojs/vercel/serverless";
+
+const env = loadEnv('production', process.cwd(), '');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), auth()]
+  output: 'server',
+  integrations: [tailwind(), react(), auth()],
+  adapter: vercel(),
+  integrations: [auth({
+    providers: [GitHub({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET
+    })]
+  })]
 });
