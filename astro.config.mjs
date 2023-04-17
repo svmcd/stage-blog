7c66d15eecfbe86da18df5c865d2ab282116a937
svmcd <spolat0750@gmail.com>
2023-04-17 12:00:44 +0200

    working auth, using discord instead of github

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import auth from "auth-astro";
 import { loadEnv } from 'vite';
-import GitHub from '@auth/core/providers/github';
+import Discord from '@auth/core/providers/discord';
 import vercel from "@astrojs/vercel/serverless";
 
 const env = loadEnv('production', process.cwd(), '');
@@ -11,13 +11,12 @@ const env = loadEnv('production', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), auth()],
+  integrations: [tailwind(), react(), auth({
+    providers: [Discord({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    })]
+  })],
   output: 'server',
   adapter: vercel(),
-  integrations: [auth({
-    providers: [GitHub({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET
-    })]
-  })]
 });
