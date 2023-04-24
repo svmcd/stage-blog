8df6fe8be44733ee9e60e56557bc8be78e9b5843
svmcd <spolat0750@gmail.com>
2023-04-24 15:04:35 +0200

    removed imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import auth from "auth-astro";
 import { loadEnv } from 'vite';
-import Discord from '@auth/core/providers/discord';
 import vercel from "@astrojs/vercel/serverless";
 
 const env = loadEnv('production', process.cwd(), '');
