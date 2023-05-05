76a5581a122763bd9e813154280a874d4af7682f
svmcd <spolat0750@gmail.com>
2023-05-05 11:16:33 +0200

    attempt 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/edge';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
