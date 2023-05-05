3ada36e6fe2bc93bffab90b9f22239f024a1f5d7
svmcd <spolat0750@gmail.com>
2023-05-05 11:18:20 +0200

    vercel edge fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/static';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
