d39bdf5e9334f781139740fbe3db86dc03c33c19
svmcd <spolat0750@gmail.com>
2023-05-05 12:42:01 +0200

    astro prerender

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/edge';
 
 import react from "@astrojs/react";
 
@@ -11,5 +11,8 @@ export default defineConfig({
   site: 'https://example.com',
   output: 'server',
   adapter: vercel(),
+  buildOptions: {
+    ssr: true,
+  },
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
