ec475119d0fc4ef94169dc4e3f163c72315b4d23
svmcd <spolat0750@gmail.com>
2023-05-05 12:48:47 +0200

    serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 import react from "@astrojs/react";
 
