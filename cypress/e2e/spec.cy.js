c627a7abaf2a1d94572846e9760006e7d8110809
svmcd <spolat0750@gmail.com>
2023-05-08 15:08:17 +0200

    auth text

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,13 +7,20 @@ describe('browser actions', () => {
     cy.url().should('include', 'stage-blog')
   })
 
+  it('should display the correct number of weekly posts (21)', () => {
+    cy.get('a[href*="blog/week"]').its('length').should('eq', 21);
+  })
+
   it('should click on week 2, and read Lorem ipsum (paywall dummy text)', () => {
     cy.get('h1').contains('Week 2').click()
     cy.get('p').contains('Lorem ipsum')
   })
 
-  it('should display the correct number of posts', () => {
-    cy.get('a[href*="blog/week"]').its('length').should('eq', 21);
+  it('gain access to the blog content by entering the secret key', () => {
+    cy.get('h1').contains('Week 2').click()
+    cy.wait(500).get('button').contains('Gain').click();
+    cy.get('input[id*="key"]').type('123')
+    cy.get('button').contains('Submit').click()
   })
 })
 
