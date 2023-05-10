ce047e736fbcecddbcc7cd09b1c6d8826970d45d
svmcd <spolat0750@gmail.com>
2023-05-10 10:38:38 +0200

    another test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,7 +12,19 @@ describe('browser actions', () => {
     cy.get('p').contains('Lorem ipsum')
   })
 
-  it('gain access to the blog content by entering the secret key', () => {
+  it('should not be able to read the blog content if the wrong key is entered', () => {
+    cy.get('h1').contains('Week 2').click()
+    cy.wait(500).get('button').contains('Gain').click();
+    cy.get('input[id*="key"]').type('somewrongkey', { delay: 50 }).wait(500)
+    cy.get('button').contains('Submit').click().wait(500)
+    cy.get('.fixed > .box-static').should('be.visible')
+    cy.get('.box-static > .fa-solid').click()
+    cy.get('p').contains('Lorem ipsum')
+    cy.get('.text-lg > .absolute').should('be.visible')
+
+  })
+
+  it('should gain access to the blog content by entering the right key', () => {
     cy.get('h1').contains('Week 2').click()
     cy.wait(500).get('button').contains('Gain').click();
     cy.get('input[id*="key"]').type(Cypress.env('key'), { delay: 50 }).wait(500)
