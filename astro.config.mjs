23267b85a2f178c7ea75264d9667319320ef55f0
svmcd <spolat0750@gmail.com>
2023-05-16 10:46:16 +0200

    removed site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
   output: 'server',
   adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind(), react()]
