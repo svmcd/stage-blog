cddc4e13b3b93c09238d32f29f50b57757d18232
svmcd <spolat0750@gmail.com>
2023-05-16 13:40:49 +0200

    vercel update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/edge';
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
